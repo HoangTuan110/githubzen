6e89dd0f90c73c2a28f5193f16c49e9026b1b1fa
Dang Hoang Tuan (Tsuki) <danghoangtuan526@proton.me>
2022-06-06 15:15:31 +0700

    Add app.py - The backbone and the fundamental of the project

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, get_flashed_messages, flash
+from mako.lookup import TemplateLookup
+from httpx import get
+
+app = Flask(__name__)
+
+templates = TemplateLookup(directories=["./templates"], module_directory="/tmp/mako_modules")
+def serve_template(name, **kwargs):
+    template = templates.get_template(name)
+    template.render(**kwargs)
+
+@app.route('/')
+def hello_world():
+    quote = get("https://api.github.com/zen")
+    if quote.is_error:
+        flash(f"Error {quote.status_code}")
+    else:
+        return serve_template("index.html", quote=quote, messages=get_flashed_messages())
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
